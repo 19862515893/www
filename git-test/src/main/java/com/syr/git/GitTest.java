32cd6e8921a5aaaa8c1fb07b3f739099fa54d3e2
songyaru <2608188901@qq.com>
2022-02-13 21:00:37 +0800

    first commit

---
+++ git-test/src/main/java/com/syr/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.syr.git;
+
+/**
+ * @author syr
+ * @date 2022/2/13 - 20:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
